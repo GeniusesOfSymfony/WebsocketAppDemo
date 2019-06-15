297d2345f43a9873fa83ff3d30c2906954b1f0ba
Michael Babker <michael.babker@gmail.com>
2019-06-15 14:42:25 -0500

    Update dependencies, bump to Symfony 4.3

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/app/example", name="homepage")
